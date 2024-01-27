b46f6a597f2b5391e2414f8ae6b90f4f517e9b77
jlesproject <143852314+jlesproject@users.noreply.github.com>
2024-01-26 22:29:23 -0500

    Update 3-s3.tf

--- 3-s3.tf
+++ 3-s3.tf
@@ -4,7 +4,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "bucketsinjaneiro"
-  acl    = "public-read"  # Set the ACL at the bucket level for public read access
 
   tags = {
     Name        = "MyBucket"
@@ -17,6 +16,37 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_access_block" {
+  depends_on = [
+    aws_s3_bucket.my_bucket,
+    aws_s3_bucket_public_access_block.public_access_block
+  ]
+
+  bucket = aws_s3_bucket.my_bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = ["arn:aws:s3:::bucketsinjaneiro/*"],
+      },
+    ],
+  })
+}
+
 resource "aws_s3_bucket_object" "dist" {
   for_each = fileset("/Users/jamieleslie/Documents/terraform/AWS/s3homepage/bucketobjects/", "*")
 
@@ -26,62 +56,47 @@ resource "aws_s3_bucket_object" "dist" {
   etag   = "fixed-etag"   # Set a fixed etag to force re-upload on every apply
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "PublicReadGetObject",
-        "Effect" : "Allow",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource": ["arn:aws:s3:::bucketsinjaneiro/*"]
-      }
-    ]
-  })
-}
-
 resource "aws_s3_bucket_object" "index" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "index.html"
 
   content = <<-EOF
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Your Exciting Website</title>
-  <style>
-    body {
-      font-family: 'Arial', sans-serif;
-      background-color: #ffeb3b; 
-      color: #e91e63;
-      text-align: center;
-    }
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Your Exciting Website</title>
+      <style>
+        body {
+          font-family: 'Arial', sans-serif;
+          background-color: #ffeb3b; 
+          color: #e91e63;
+          text-align: center;
+        }
 
-    h1 {
-      font-size: 3em; 
-      margin-bottom: 20px; 
-    }
+        h1 {
+          font-size: 3em; 
+          margin-bottom: 20px; 
+        }
 
-    p {
-      font-size: 1.5em; 
-    }
+        p {
+          font-size: 1.5em; 
+        }
 
-    img {
-      width: 35%; 
-      border-radius: 10px; 
-      margin-top: 20px; 
-    }
-  </style>
-</head>
-<body>
-  <h1>She told me to tell you...</h1>
-  <p>Don't Stop Grinding!</p>
-  <img src="https://bucketsinjaneiro.s3.amazonaws.com/brazil.gif" alt="Brazil Image">
-</body>
-</html>
+        img {
+          width: 35%; 
+          border-radius: 10px; 
+          margin-top: 20px; 
+        }
+      </style>
+    </head>
+    <body>
+      <h1>She told me to tell you...</h1>
+      <p>Don't Stop Grinding!</p>
+      <img src="https://bucketsinjaneiro.s3.amazonaws.com/brazil.gif" alt="Brazil Image">
+    </body>
+    </html>
   EOF
 
   content_type = "text/html"    # Set the content type of the index.html file
 }
+
